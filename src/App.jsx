a953e15e9b7f28d5557c815c2857b6f04c3c471f
Juan Mato <jjmatotorres@gmail.com>
2023-02-24 22:22:31 +0100

    feat: all changes on API

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import BookCreate from './components/BookCreate'
 import BooksList from './components/BooksList'
 
-const API_BOOKS_ENDPOINT = 'http://localhost:3001/books'
+const API_BOOKS_ENDPOINT = 'http://localhost:3001/books/'
 
 const fetchBooks = async () => {
   return await fetch(API_BOOKS_ENDPOINT)
@@ -22,6 +22,24 @@ const postBooks = async (title) => {
   return await response.json()
 }
 
+const updateBook = async (id, title) => {
+  const response = await fetch(API_BOOKS_ENDPOINT + id, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ title })
+  })
+  return await response.json()
+}
+
+const deleteBook = async (id) => {
+  const response = await fetch(API_BOOKS_ENDPOINT + id, {
+    method: 'DELETE'
+  })
+  return await response.json()
+}
+
 function App () {
   const [books, setBooks] = useState([])
 
@@ -35,11 +53,13 @@ function App () {
     setBooks([...books, bookRecorded])
   }
 
-  const deleteBooks = (idForDelete) => {
+  const deleteBooks = async (idForDelete) => {
+    await deleteBook(idForDelete)
     setBooks(books.filter(({ id }) => id !== idForDelete))
   }
 
-  const editBook = ({ id, newTitle }) => {
+  const editBook = async ({ id, newTitle }) => {
+    await updateBook(id, newTitle)
     setBooks(books.map(book => book.id === id ? { ...book, title: newTitle } : { ...book }))
   }
 
